6148b77e6a18b17c405f8eb058b336a73567fa97
ilyasipek <ilyasipek1997@gmail.com>
2023-11-12 13:37:35 +0300

    Add more git aliases

--- .zsh_aliases.zsh
+++ .zsh_aliases.zsh
@@ -2,9 +2,24 @@
 alias gw='./gradlew'
 
 # Git
-alias gir='git rebase -i HEAD~$1'
-alias gski='git stash save $1 --keep-index -u'
-alias gstall='git stash save $1 -u'
+function gir() {
+    git rebase -i HEAD~"$1"
+}
+alias gir='gir'
+
+alias gski='git stash save --keep-index -u'
+alias gsall='git stash save -u'
+alias gslist='git stash list'
 alias gs='git status'
+
+alias gc='git commit'
+alias gamend='git commit --amend --no-edit'
+
+alias gp='git push'
 alias gpf='git push --force-with-lease'
+
 alias gcp='git cherry-pick'
+alias gco='git checkout'
+alias gcob='git checkout -b'
+alias gbr='git branch'
+alias gbrd='git branch -d'
