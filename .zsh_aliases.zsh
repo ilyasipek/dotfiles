4e4f528c587bf76b3a4b66f7a5cb97d6f4ef62ef
ilyasipek <ilyasipek1997@gmail.com>
2023-11-12 12:19:00 +0300

    Add gradlew and git aliases

---
+++ .zsh_aliases.zsh
@@ -0,0 +1,10 @@
+# Gradle
+alias gw='./gradlew'
+
+# Git
+alias gir='git rebase -i HEAD~$1'
+alias gski='git stash save $1 --keep-index -u'
+alias gstall='git stash save $1 -u'
+alias gs='git status'
+alias gpf='git push --force-with-lease'
+alias gcp='git cherry-pick'
